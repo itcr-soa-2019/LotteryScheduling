af3bf5d7c22ede5ec8fd5cc907067ffcd3ab0899
Manuel Figueroa <mfigueroacr@gmail.com>
2019-02-20 21:11:56 -0600

    Include progress report

--- src/jobs/piCalc.c
+++ src/jobs/piCalc.c
@@ -17,7 +17,7 @@ void piCalculation(task_t *task) {
     for(long i = 0; i < workUnits;  i++)
     {
          calculateWorkUnit((i*TermsPerUnit)+startPoint, &partialSum);
-         //task->progress = (double)i + 1.0 / (double) workUnits; // Calculates the current progress for this task
+         task->progress = (double)i + 1.0 / (double) workUnits; // Calculates the current progress for this task
     }
     partialValues[id]=partialSum;
 }
