5be9e3d962794d9d6f90050c90205351093dfc08
Fabiola <fabiolaeh9414@gmail.com>
2019-02-24 20:51:06 -0600

    Revert "Scheduler methods have been added"
    This reverts commit dc77719d6edc4314f4691722bbad0e71c0b022b5.
    
    This will be moved to scheduler branch until we merge changes.

--- src/scheduler/scheduler.h
+++ src/scheduler/scheduler.h
@@ -1,43 +1,12 @@
 #ifndef scheduler_h
 #define scheduler_h
-#include "../threads/thread.h"
-#include "../scheduler/task.h"
 
-
-typedef struct
+typedef struct scheduler_t
 {
    int operationMode; // expropiative = 1   nonexpropiative = 0
    task_list_t *taskList;
    task_t *currentTask; // task being executed
-   thread_t** threads;
-    long numThreads;
-    long currentThread;
-    int preemptive;
-    long completedThreads;
-    int playingTickets;
-    double yieldPercentage;
-    sigjmp_buf context;
-    int scheduleComplete;
-
-}scheduler_t;
-
-extern scheduler_t* Scheduler;
-
-
-int Scheduler_SaveOwnContext(scheduler_t* Scheduler); //saves the context of the scheduler
-void Scheduler_ResumesOwnContext(scheduler_t* Scheduler); //resumes the context of the scheduler
-void Scheduler_Init(long numThreads, void* function, int preemptive, double yiedlPercentage, long* tickets, long* work);
-void Scheduler_Free(scheduler_t* Scheduler);
-int  Scheduler_SaveThread(scheduler_t* Scheduler); //saves current thread context
-void Scheduler_ResumeThread(scheduler_t* Scheduler); //resumes current thread
-void Scheduler_Schedule(scheduler_t* Scheduler);
-void Scheduler_ThreadCompletes(scheduler_t* Scheduler);
-void Scheduler_SwitchThreads(scheduler_t* Scheduler);
-void Scheduler_SaveResult(scheduler_t* Scheduler, double result);
-long Scheduler_GetWorkOfCurrentThread(scheduler_t* Scheduler);
-
-
-
+} scheduler_t;
 
 // Initialize scheduling instance
 void initScheduling(int operationMode, task_list_t *taskList);
@@ -49,7 +18,4 @@ task_t *allocateNextTask();
 void completeTask(int pid);
 
 
-
-#endif 
-
-
+#endif
