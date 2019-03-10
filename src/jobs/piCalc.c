9119871bce735b9999a86274fde03c10f3e12de6
Nathalia3 <natgondu@gmail.com>
2019-03-09 21:41:26 -0600

    reportProgress template method

--- src/jobs/piCalc.c
+++ src/jobs/piCalc.c
@@ -24,6 +24,28 @@ void piCalculation(task_t *task, void (*reportProgress)(double)) {
     partialValues[id]=partialSum;
 }
 
+// Used by the thread to see if the workUnit are completed or need to assign a new thread 
+void  reportProgress(progress) {
+    // check if progress is 100% and delete from task list
+    task_t *task;
+    int id = task->thread->id;
+    if (task->currentThread != NULL && task->progress == 1) {
+        printf("Task COMPLETED-> %d.\n", task->id);
+        deallocateCurrentTask();
+    }else{
+        while (progress != 1){
+            saveThread(task);
+            // initLotterySchedule() or createThread or appendThread ?????
+
+        }
+
+    }
+
+   
+    
+}
+
+
 /**
  * Calculates a single workUnit.
  * A Work unit is equivalent to 50 terms of the 
